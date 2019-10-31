9e800e6d8454360aef20b92662fc8e619bd68d5b
daltschu22 <daltschu@broadinstitute.org>
2019-10-31 15:10:48 -0400

    List bucket object metadata

--- list_bucket.py
+++ list_bucket.py
@@ -5,6 +5,7 @@ import argparse
 from google.cloud import storage
 import google.cloud.exceptions
 from google.oauth2 import service_account
+import json
 
 PROJECT = os.environ.get('GCP_PROJECT')
 # SERVICE_ACCOUNT = os.environ.get('')
@@ -35,6 +36,18 @@ def get_bucket(storage_client, bucket):
     except google.cloud.exceptions.NotFound:
         print('ERROR: Requested bucket doesnt exist!')
 
+def billing_upload_byfs_to_bigquery_historical(storage_client, bucket, file_to_upload):
+    """Import the provided billing dict to bigquery table for historical purposes."""
+
+
+    # print("\nUploading filesystem data to bigquery...")
+    # # Insert the newline delimeted json file to bucket, cloud function will grab this and put in bq
+    # g.storage().insert_object(
+    #     'bits-bitstore-json-billing-data-ingest',
+    #     'broad_bitstore_app/bits_billing_byfs_bitstore_historical.json',
+    #     '\n'.join(row_list)
+    # )
+
 def main():
     args = parse_arguments()  # Parse arguments
 
@@ -62,21 +75,34 @@ def main():
 
     blob_list = storage_client.list_blobs(bucket_name)
 
-    for blob in blob_list:
-        print(blob)
-        quit()
+    with open('{}.list'.format(bucket_name), 'w') as outfile:
+        for blob in blob_list:
 
-    # print(working)
+            blob_dir = {}
+            blob_dir['bucket'] = blob.bucket.name
+            blob_dir['time_created'] = blob.time_created.strftime('%Y-%m-%d %H:%M:%S')
+            blob_dir['time_updated'] = blob.updated.strftime('%Y-%m-%d %H:%M:%S')
+            blob_dir['storage_class'] = blob.storage_class
+            blob_dir['content_type'] = blob.content_type
+            blob_dir['content_language'] = blob.content_language
+            blob_dir['hash_md5'] = blob.md5_hash
+            blob_dir['hash_crc'] = blob.crc32c
+            blob_dir['etag'] = blob.etag
+            blob_dir['generation'] = blob.generation
+            blob_dir['metageneration'] = blob.metageneration
+            blob_dir['size'] = blob.size
+            blob_dir['name'] = blob.name
+            blob_dir['url'] = blob.path
+            blob_dir['id'] = blob.id
 
+            print(blob_dir)
 
+            blob_json = json.dumps(blob_dir)
 
-    # new_blob = working_bucket.blob('testing123')
+            outfile.write('{}\n'.format(blob_json))
 
-    # print("uploading object to bucket {}".format(stuff))
-
-    # new_blob.upload_from_string(stuff)
-
-
+            quit()
 
 if __name__ == "__main__":
     main()
+
