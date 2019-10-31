cc57d373706efcd335866a2a01677a6322ef9e2b
daltschu22 <daltschu@broadinstitute.org>
2019-10-31 13:37:59 -0400

    some stuff

--- list_bucket.py
+++ list_bucket.py
@@ -11,16 +11,19 @@ PROJECT = os.environ.get('GCP_PROJECT')
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="List bucket and dump ls info to bigquery")
-    parser.add_argument('bucket_name', metavar='gs://bucket-name/', help="Bucket name in google")
+    parser.add_argument('bucket_name', metavar='bucket-name', help="Bucket name in google")
     parser.add_argument('--project', dest='project_override', required=False, help="if you want to manually define a project")
-    parser.add_argument('--service-account', dest='service_account', required=False, help="service account json")
+    parser.add_argument('--service-account-json', dest='service_account_json', required=False, help="service account json")
 
     return parser.parse_args()
 
-def initiate_storage_client(credentials):
+def initiate_storage_client(credentials=None):
     # print("Initializing google storage client...")
     try:
-        storage_client = storage.Client(project=PROJECT)
+        if credentials:
+            storage_client = storage.Client(project=PROJECT, credentials=credentials)
+        else:
+            storage_client = storage.Client(project=PROJECT)
         return storage_client
     except Exception as e:
         print("ERROR: Could not connect to storage API!: {}".format(e))
@@ -37,27 +40,30 @@ def main():
 
     bucket_name = args.bucket_name
     project_override = args.project_override
-    service_account = args.service_account
+    service_account_json = args.service_account_json
 
     if project_override:
         PROJECT = project_override
 
-    if service_account:
-        SERVICE_ACCOUNT = service_account
-
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT,
-        scopes=["https://www.googleapis.com/auth/cloud-platform"],
-    )
-
-    # Initialize the storage client
-    storage_client = initiate_storage_client(credentials)
+    if service_account_json:
+        SERVICE_ACCOUNT = service_account_json
+        credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"],
+        )
+        # Initialize the storage client
+        storage_client = initiate_storage_client(credentials)
+    else:
+        # Initialize the storage client
+        storage_client = initiate_storage_client()
 
     service_account_name = storage_client.get_service_account_email(PROJECT)
 
     stuff = "{} - {}".format(bucket_name, service_account_name)
 
-    new_blob = storage_client.bucket.blob('testing123')
+    working_bucket = get_bucket(storage_client, bucket_name)
+
+    new_blob = working_bucket.blob('testing123')
 
     print("uploading object to bucket {}".format(stuff))
 
