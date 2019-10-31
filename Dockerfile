146df122c7d1c07104b83460fd9352f3d8dfeec6
daltschu22 <daltschu@broadinstitute.org>
2019-10-31 12:30:52 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM python:3.7-alpine
+
+WORKDIR /usr/src
+
+# Copies the code
+COPY requirements.txt /usr/src/requirements.txt
+
+RUN apk update \
+    && apk add bash gcc git g++ libxml2-dev libxslt-dev musl-dev \
+    && pip install -r requirements.txt \
+    && rm -rf /tmp/* \
+    && rm -rf /var/cache/apk/* \
+    && rm -rf /var/tmp/*
+
+# Installs google cloud sdk, this is mostly for using gsutil to export model.
+RUN wget -nv \
+    https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz && \
+    mkdir /root/tools && \
+    tar xvzf google-cloud-sdk.tar.gz -C /root/tools && \
+    rm google-cloud-sdk.tar.gz && \
+    /root/tools/google-cloud-sdk/install.sh --usage-reporting=false \
+        --path-update=false --bash-completion=false \
+        --disable-installation-options && \
+    rm -rf /root/.config/* && \
+    ln -s /root/.config /config && \
+    # Remove the backup directory that gcloud creates
+    rm -rf /root/tools/google-cloud-sdk/.install/.backup
+
+# Path configuration
+ENV PATH $PATH:/root/tools/google-cloud-sdk/bin
+# Make sure gsutil will use the default service account
+RUN echo '[GoogleCompute]\nservice_account = default' > /etc/boto.cfg
+
+# Copies the code
+COPY list_bucket.py /usr/src/list_bucket.py
+
+# Sets up the entry point to invoke the trainer.
+ENTRYPOINT ["python", "/usr/src/list_bucket.py"]
---
+++ list_bucket.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+from google.cloud import storage, logging
+import google.cloud.exceptions
+
+PROJECT = os.environ.get('GCP_PROJECT')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="List bucket and dump ls info to bigquery")
+    parser.add_argument('bucket_name', metavar='gs://bucket-name/', help="Bucket name in google")
+
+    return parser.parse_args()
+
+def initiate_storage_client():
+    # print("Initializing google storage client...")
+    try:
+        storage_client = storage.Client(project=PROJECT)
+        return storage_client
+    except Exception as e:
+        logging.warn("ERROR: Could not connect to storage API!: {}".format(e))
+
+def get_bucket(storage_client, bucket):
+    try:
+        bucket_object = storage_client.get_bucket(bucket)
+        return bucket_object
+    except google.cloud.exceptions.NotFound:
+        logging.warn('ERROR: Requested bucket doesnt exist!')
+
+def update_bucket_sizes(data, context):
+    # Initialize the storage client
+    storage_client = initiate_storage_client()
+
+    args = parse_arguments()  # Parse arguments
+
+    bucket_name = args.bucket_name
+
+    service_account_name = storage_client.get_service_account_email()
+
+    stuff = "{} - {}".format(bucket_name, service_account_name)
+
+    new_blob = storage_client.bucket.blob('testing123')
+
+    print("uploading object to bucket {}".format(stuff))
+
+    new_blob.upload_from_string(stuff)
